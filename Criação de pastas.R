883a275f9da00681f3588f90ce09b05787edc983
Alexandre Sanches <al.sanches@icloud.com>
2022-01-11 15:07:31 -0300

    Inclusão de mais artigos da RBE

--- Criação de pastas.R
+++ Criação de pastas.R
@@ -1,17 +1,14 @@
 setwd("~/Documents/Projetos/NLP")
 
 anos <- c(1948:2021)
-versao <- c(1:75)
 numero <- c(1:4)
 
 for (i in anos) {
-  for (j in versao) {
-    for (z in numero) {
+  for (j in numero) {
       
-      caminho <- paste("PDF", i, j, z, sep = "/")
+      caminho <- paste("PDF", i, j, sep = "/")
       dir.create(path = caminho, recursive = TRUE)
       
-    }
   }
 }
 
